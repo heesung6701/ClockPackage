cf2ac7c57d25be5a3f62f935586c8ac296d5b0a1
hee.youn <heesung6701@naver.com>
2021-08-22 20:32:42 +0900

    Implement MainActivity.

--- app/src/main/java/com/quokkaman/android/app/MainActivity.kt
+++ app/src/main/java/com/quokkaman/android/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.quokkaman.android.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.quokkaman.android.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_alarm, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_add ->
+                Toast.makeText(this, getString(R.string.add), Toast.LENGTH_SHORT).show()
+            R.id.menu_edit ->
+                Toast.makeText(this, getString(R.string.edit), Toast.LENGTH_SHORT).show()
+            R.id.menu_sort ->
+                Toast.makeText(this, getString(R.string.sort), Toast.LENGTH_SHORT).show()
+            R.id.menu_inquire ->
+                Toast.makeText(this, getString(R.string.inquire), Toast.LENGTH_SHORT).show()
+            R.id.menu_setting ->
+                Toast.makeText(this, getString(R.string.setting), Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
