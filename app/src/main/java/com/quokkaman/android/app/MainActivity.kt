534de29d5fa93962a78337e9afd8735901e10959
윤희성 <heesung6701@naver.com>
2021-08-28 13:52:29 +0900

    implement Dynamically Toolbar

--- app/src/main/java/com/quokkaman/android/app/MainActivity.kt
+++ app/src/main/java/com/quokkaman/android/app/MainActivity.kt
@@ -1,42 +1,86 @@
 package com.quokkaman.android.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.quokkaman.android.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var toolbar: Toolbar
+    private lateinit var collapsingToolbarLayout: CollapsingToolbarLayout
+
+    private val navChangeListener =
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+            collapsingToolbarLayout.title = destination.label
+            Log.d("HSSS", "DestinationChanged : " + destination.label)
+            when (destination.id) {
+                R.id.navigation_alarm -> {
+                    toolbar.menu.clear()
+                    toolbar.inflateMenu(R.menu.menu_alarm)
+                }
+                R.id.navigation_world_clock -> {
+                    toolbar.menu.clear()
+                }
+                R.id.navigation_stopwatch -> {
+                    toolbar.menu.clear()
+                }
+                R.id.navigation_timer -> {
+                    toolbar.menu.clear()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
-
+        toolbar = binding.toolbar
+        collapsingToolbarLayout = binding.toolbarLayout
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_alarm, R.id.navigation_world_clock, R.id.navigation_stopwatch, R.id.navigation_timer
+                R.id.navigation_alarm,
+                R.id.navigation_world_clock,
+                R.id.navigation_stopwatch,
+                R.id.navigation_timer
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(navChangeListener)
+    }
+
+    override fun onPause() {
+        navController.removeOnDestinationChangedListener(navChangeListener)
+        super.onPause()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_alarm, menu)
-        return super.onCreateOptionsMenu(menu)
+        navChangeListener.onDestinationChanged(navController, navController.currentDestination!!, null)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
