05bf354fbf6bc39e942a48bfd06d0c906495d79e
윤희성 <heesung6701@naver.com>
2021-08-29 13:45:44 +0900

    Add Holiday field on AlarmDayPlan

--- app/src/main/java/com/quokkaman/android/app/common/data/AlarmDayPlan.kt
+++ app/src/main/java/com/quokkaman/android/app/common/data/AlarmDayPlan.kt
@@ -2,10 +2,11 @@ package com.quokkaman.android.app.common.data
 
 import java.util.*
 
-class AlarmDayPlan(private val daySet: EnumSet<DayOfWeek>) : AlarmPlan {
+class AlarmDayPlan(private val daySet: EnumSet<DayOfWeek>, private val offOnHoliday : Boolean = false) : AlarmPlan {
     override fun valid(date: Date): Boolean {
         val calendar = Calendar.getInstance()
         calendar.time = date
+        // TODO check is Holiday.
         val day = calendar.get(Calendar.DAY_OF_WEEK)
         return daySet.contains(DayOfWeek.create(day))
     }
