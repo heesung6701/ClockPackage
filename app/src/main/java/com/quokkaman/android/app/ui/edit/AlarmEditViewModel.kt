0a489882126d7d7103f0594e50a43cc6741bfe78
윤희성 <heesung6701@naver.com>
2021-09-09 23:28:00 +0900

    implements read input of title and time

--- app/src/main/java/com/quokkaman/android/app/ui/edit/AlarmEditViewModel.kt
+++ app/src/main/java/com/quokkaman/android/app/ui/edit/AlarmEditViewModel.kt
@@ -1,27 +1,41 @@
 package com.quokkaman.android.app.ui.edit
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.quokkaman.android.app.common.data.Alarm
 import com.quokkaman.android.app.common.data.Time
 
-class AlarmEditViewModel(val generator: (Time, String) -> Alarm) : ViewModel() {
+class AlarmEditViewModel(private val editPresenter: AlarmEditPresenter) : ViewModel() {
 
+    val timeLiveData = MutableLiveData<Time>()
+    val titleLiveData = MutableLiveData<String>()
+
+    val updateTime = fun (time: Time) {
+        timeLiveData.value = time
+    }
+
+    init {
+        timeLiveData.value = Time.create(6, 0)
+    }
 
     fun onCancel() {
+        editPresenter.finish()
     }
 
     fun onSave() {
-        val alarm = generator(Time.create(1, 0), "")
-        Log.d(TAG, alarm.toString())
+        val time = timeLiveData.value ?: return
+        val title = titleLiveData.value ?: ""
+        val alarm = editPresenter.generateAlarm(time, title)
+        editPresenter.makeToast(alarm.toString())
+        editPresenter.finish()
     }
 
-    class Factory(private val generator: (Time, String) -> Alarm) :
+    class Factory(private val editPresenter: AlarmEditPresenter) :
         ViewModelProvider.Factory {
 
         override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-            AlarmEditViewModel(generator) as T
+            AlarmEditViewModel(editPresenter) as T
     }
     companion object {
         private val TAG = this::class.java.simpleName
