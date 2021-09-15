042ad4cb1961b0e930075c07d0879ca6584f06af
hee.youn <heesung6701@naver.com>
2021-09-16 00:13:10 +0900

    년도가 다른 경우 연도 표기 기능 추가

--- app/src/main/java/com/quokkaman/android/app/common/data/AlarmDatePlan.kt
+++ app/src/main/java/com/quokkaman/android/app/common/data/AlarmDatePlan.kt
@@ -11,14 +11,23 @@ class AlarmDatePlan(private val date: Date) : AlarmPlan {
     }
 
     override fun getTitle(): String {
-        val dateFormat = SimpleDateFormat("MM월 dd일 (E)", Locale.KOREA)
+        val year = Calendar.getInstance(Locale.KOREA).apply {
+            time = date
+        }.get(Calendar.YEAR)
 
         val calendar = Calendar.getInstance(Locale.KOREA)
+        val todayYear = calendar.get(Calendar.YEAR)
+        if (year != todayYear) {
+            val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일 (E)", Locale.KOREA)
+            return dateFormat.format(date.time)
+        }
+        val dateFormat = SimpleDateFormat("MM월 dd일 (E)", Locale.KOREA)
+
         val todayStr = dateFormat.format(calendar.time)
         calendar.add(Calendar.DAY_OF_YEAR, 1)
         val tomorrowStr = dateFormat.format(calendar.time)
         val dateStr = dateFormat.format(date.time)
-        return when(dateStr) {
+        return when (dateStr) {
             todayStr -> "오늘 - "
             tomorrowStr -> "내일 - "
             else -> ""
